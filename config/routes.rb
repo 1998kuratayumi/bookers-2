a6982e48a017c3f0ea49535a9acd808d99a2f22e
1998kuratayumi <yu3_backstroke@icloud.com>
2021-05-31 11:58:58 +0000

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
  devise_for :users
   root to: "homes#top"
   get '/home/about', to: "homes#about"
-  
+
 
   resources :books
   resources :users
+  resource :favorites, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
